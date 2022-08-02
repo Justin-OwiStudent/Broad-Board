b1ed4b003e49d18881becf05c0b7c413f6aa4662
Justin-OwiStudent <200100@virtualwindow.co.za>
2022-08-02 21:08:31 +0200

    Admin
    made admin page and styled it, still need add the navigation and order page

--- client/src/components/Admin/Admin.js
+++ client/src/components/Admin/Admin.js
@@ -13,6 +13,30 @@ const Admin = () => {
 
                 <div className={classes.MainManage}>
                     <div className={classes.item}>
+                        <h1 className={classes.orderName}>hehes</h1>
+                        <h1 className={classes.orderDetails}>hehes</h1>
+                        <h1 className={classes.orderStatus}>hehes</h1>
+
+                    <div className={classes.actions}>
+                        <div className={classes.done}></div>
+                        <div className={classes.delete}></div>
+                    </div>
+
+                    </div>
+
+                    <div className={classes.item}>
+                        <h1 className={classes.orderName}>hehes</h1>
+                        <h1 className={classes.orderDetails}>hehes</h1>
+                        <h1 className={classes.orderStatus}>hehes</h1>
+
+
+                    <div className={classes.actions}>
+                        <div className={classes.done}></div>
+                        <div className={classes.delete}></div>
+                    </div>
+                        
+
+                        
 
                     </div>
                 </div>
